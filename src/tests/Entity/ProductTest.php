c1f3df2ebec0d27666acd9a1081b918093e1bb0c
Eric Amshukov <eamshukov@gmail.com>
2019-03-27 15:13:18 -0400

    unit test wip

---
+++ src/tests/Entity/ProductTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Product;
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function testAdd()
+    {
+        $this->assertEquals(42, 42);
+    }
+}
